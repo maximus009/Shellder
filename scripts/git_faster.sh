eda8d6e25e59fe1ca86d7e3d0cc55745090bf70f
maximus009 <kssrisingavenger@gmail.com>
2017-07-29 10:26:52 -0400

    adding git faster package

---
+++ scripts/git_faster.sh
@@ -0,0 +1,11 @@
+# At times when even auto complete on your
+# shell/terminal is not enough and
+# you are high on gitting
+
+# Add the following lines to your
+# bash profile or bashrc 
+# and git faster than before
+
+alias Push='git push origin $1'
+alias Status='git status'
+alias Pull='git pull $1'
